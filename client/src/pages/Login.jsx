81c4ee24874b89a63bd96f27682b4a7c2d4027d8
Soumyadip Mukherjee <139647504+Soumya-mukherjee131@users.noreply.github.com>
2024-10-05 20:51:14 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -30,7 +30,8 @@ const Login = () => {
     // console.log(user);
 
     try{
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      //http://localhost:5000/api/auth/login
+      const response = await fetch("https://soumya-admindashbord-backend.onrender.com", {
         method:"POST",
         headers:{
           'Content-Type':'application/json',
